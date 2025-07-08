e223052d38cb3adeb67fa474b4d65b01de3d5923
naim79992 <mhnaim79992@gmail.com>
2025-07-08 22:05:35 +0600

    CRUD

--- ModelViewController/src/main/java/com/example/ModelViewController/controller/ProductController.java
+++ ModelViewController/src/main/java/com/example/ModelViewController/controller/ProductController.java
@@ -3,11 +3,13 @@ package com.example.ModelViewController.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ModelViewController.model.Product;
@@ -28,8 +30,17 @@ public class ProductController {
         return service.getProductById(prodId);
     }
 
-    @PostMapping("/product")
+    @PostMapping("/products")
     public void addProduct(@RequestBody Product prod){
         service.addProduct(prod);
+    }
+    //update
+    @PutMapping("/products")
+    public void update(@RequestBody Product prod){
+          service.updateProduct(prod);
+    }
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
 }
