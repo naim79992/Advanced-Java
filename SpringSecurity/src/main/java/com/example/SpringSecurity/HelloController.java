5b3e030fbd2864b4d67514f2eb6ce7c239df0162
naim79992 <mhnaim79992@gmail.com>
2025-07-24 10:30:09 +0600

    CSRF- Implement HelloController with session ID response and add security user credentials in application properties

--- SpringSecurity/src/main/java/com/example/SpringSecurity/HelloController.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/HelloController.java
@@ -3,10 +3,13 @@ package com.example.SpringSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
+
     @GetMapping("/")
-    public String check(){
-       return "Testing Security";
+    public String check(HttpServletRequest request) {
+        return "Testing Security " + request.getSession().getId();
     }
 }
